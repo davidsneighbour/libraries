c71eea06da66c9083b4ff323f6263d46841f7a48
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-06-23 21:21:17 +0700

    chore: add update of popperjs to build script

--- bootstrap5/bin/release-hook-prerelease.sh
+++ bootstrap5/bin/release-hook-prerelease.sh
@@ -2,9 +2,15 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+npm-check-updates -u
+npm install
+cp -R node_modules/@popperjs/core assets/libs/popperjs
+git add assets/libs/popperjs
+git commit -m "chore(deps): update popperjs library"
+
 if test -f "$SCRIPTPATH"/replacements; then
   while read -ra __; do
-    go mod edit -dropreplace ${__[0]}
+    go mod edit -dropreplace "${__[0]}"
   done < "$SCRIPTPATH"/replacements
 fi
 
@@ -16,6 +22,6 @@ git add go.sum
 
 if test -f "$SCRIPTPATH"/replacements; then
   while read -ra __; do
-    go mod edit -replace ${__[0]}=${__[1]}
+    go mod edit -replace "${__[0]}"="${__[1]}"
   done < "$SCRIPTPATH"/replacements
 fi
